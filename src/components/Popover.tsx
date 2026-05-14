566af862ab55addd169928a52ccab7f0f7604963
Vladimir Kharlampidi <nolimits4web@gmail.com>
2026-05-14 14:01:34 -0400

    feat: switch Popover to default fixed position

--- src/components/Popover.tsx
+++ src/components/Popover.tsx
@@ -521,7 +521,6 @@ const PopoverInner = (props: PopoverInnerProps) => {
   const [mainProp, crossProp] = positionConfig.offsetProperties;
 
   const popoverStyle = {
-    position: 'absolute',
     positionAnchor: anchorName,
     positionArea: positionConfig.area,
     positionTryFallbacks: 'flip-block, flip-inline, flip-block flip-inline',
@@ -583,7 +582,7 @@ const PopoverInner = (props: PopoverInnerProps) => {
           }}
           style={popoverStyle}
           className={cn(
-            'pointer-events-auto z-50 flex w-40 max-w-[calc(100vw-16px)] rounded-cladd-popover shadow-cladd-popover transition-[opacity,transform,scale] duration-0',
+            'pointer-events-auto fixed z-50 flex w-40 max-w-[calc(100vw-16px)] rounded-cladd-popover shadow-cladd-popover transition-[opacity,transform,scale] duration-0',
             opened && 'scale-100 opacity-100 ease-[cubic-bezier(0,1,0,1.025)]',
             (phase === 'opened' || (phase === 'opening' && opened)) &&
               'duration-400',
